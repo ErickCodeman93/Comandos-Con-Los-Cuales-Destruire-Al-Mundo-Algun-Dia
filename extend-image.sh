4f6e40b724e4001e7b316471da96cc3ae35a1578
Erick Alva <erick@Mac-mini-de-Erick.local>
2018-10-02 12:28:45 -0500

    resize image

---
+++ extend-image.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+#  ██████╗ ██████╗ ██████╗ ███████╗███╗   ███╗ █████╗ ███╗   ██╗
+# ██╔════╝██╔═══██╗██╔══██╗██╔════╝████╗ ████║██╔══██╗████╗  ██║
+# ██║     ██║   ██║██║  ██║█████╗  ██╔████╔██║███████║██╔██╗ ██║
+# ██║     ██║   ██║██║  ██║██╔══╝  ██║╚██╔╝██║██╔══██║██║╚██╗██║
+# ╚██████╗╚██████╔╝██████╔╝███████╗██║ ╚═╝ ██║██║  ██║██║ ╚████║
+#  ╚═════╝ ╚═════╝ ╚═════╝ ╚══════╝╚═╝     ╚═╝╚═╝  ╚═╝╚═╝  ╚═══╝
+
+# apt-get install imagemagick
+
+extension="jpg"
+# extension="png"
+now="IMAGEMAGICK"
+#width="1100"
+
+for image in ~/Downloads/img/img/servicios/*.jpg;
+# for image in ~/Desktop/images/*.png;
+do
+	echo "$image <=> ${image%.*}$now.$extension"
+    convert "$image" -gravity center -extent 1100x800 "${image%.*}$now.$extension"
+	
+
+	# Optional
+	# rm "$image"
+done;
+
+# Cache
+rm -rf /tmp/magick-*
